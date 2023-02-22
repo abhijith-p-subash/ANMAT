ae38a2c801b3529eb889bb32e8402d8ac44696b6
sixbeeshades <abhijith.p.subash@gmail.com>
2023-02-22 23:06:05 +0530

    fix

--- src/app/layout/layouts/vertical/modern/modern.module.ts
+++ src/app/layout/layouts/vertical/modern/modern.module.ts
@@ -5,7 +5,6 @@ import { HeaderModule } from '../../../common/header/header.module';
 import { FooterModule } from 'src/app/layout/common/footer/footer.module';
 import { MenuModule } from 'src/app/layout/common/menu/menu.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
